74c5b260d53619551ca03668091e54b2cbdb4d9f
Philipp Panzer <4118161+yodaaut@users.noreply.github.com>
2020-04-20 04:58:33 +0200

    add BackgroundComponent and MainComponent

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './background/background.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
