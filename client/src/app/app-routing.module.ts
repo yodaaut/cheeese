49cb189f3dc76d3720670c0109134f017dc4a43f
Philipp Panzer <4118161+yodaaut@users.noreply.github.com>
2020-04-20 04:55:26 +0200

    add route main and background

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BackgroundComponent } from './background/background.component';
+import { MainComponent } from './main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'background', component: BackgroundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
